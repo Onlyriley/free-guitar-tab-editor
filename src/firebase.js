1f80e8a42ccc43c49f719d4994aa4ebc485bfecb
Riley Simmons <rcs21024@huskies.bloomu.edu>
2026-04-02 14:44:53 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/
 import { useState } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmBz-gIHdG6AJh24-WaD08Rh4vALngosM",
-  authDomain: "tabby-b0189.firebaseapp.com",
-  projectId: "tabby-b0189",
-  storageBucket: "tabby-b0189.firebasestorage.app",
-  messagingSenderId: "40876582879",
-  appId: "1:40876582879:web:2eef16a3425c62ce83066a"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const provider = new GoogleAuthProvider();
