24a384ab40a4dae4a0e58329b2da11f79fdbc0fe
Riley Simmons <rcs21024@huskies.bloomu.edu>
2026-04-03 11:33:13 -0600

    Node

--- src/firebase.js
+++ src/firebase.js
@@ -7,8 +7,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const provider = new GoogleAuthProvider();
